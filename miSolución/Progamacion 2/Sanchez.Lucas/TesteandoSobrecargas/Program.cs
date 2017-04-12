4812ea867b73cc381f6ed44bfd5ed23edf9e8525
lucasgabrielsanchez <sanchez_lukas@hotmail.com>
2017-04-12 00:29:24 -0300

    agregando

---
+++ miSolución/Progamacion 2/Sanchez.Lucas/TesteandoSobrecargas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteandoSobrecargas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSobrecarga testing = new TestSobrecarga(); //constructor por default
+
+            testing.Inicializar("Hola");
+            testing.Inicializar(5);
+            testing.Inicializar(DateTime.Now);
+
+            testing = new TestSobrecarga(5, "pepe"); //constructor sobrecargado con int y string como parametros
+
+            testing = new TestSobrecarga("carlitos"); //constructor sobrecargado con string como parametro
+
+            Console.WriteLine ("{0}", testing.Mostrar());
+
+            Console.ReadLine();
+        }
+
+    }
+}
