118cf63689a9e0d2542ad73c5440b4691fc99206
lucasgabrielsanchez <sanchez_lukas@hotmail.com>
2017-04-12 00:31:42 -0300

    agregando

---
+++ miSolución/Progamacion 2/Sanchez.Lucas/TestLibreria/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace TestLibreria
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            TestSobrecarga xD = new TestSobrecarga(18,"pepitosuarez");
+
+            Console.WriteLine("{0}", xD.Mostrar());
+
+            Console.ReadLine();
+
+            xD.Inicializar("ahrre");
+            xD.Inicializar(DateTime.Now);
+            xD.Inicializar(468);
+
+            Console.WriteLine("{0}", xD.Mostrar());
+
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
