b83a6df3d73652029d4fc72b67c6f3448f9c3cad
alumno <alumno>
2017-06-05 22:10:59 -0300

    TP3 empezando

---
+++ Archivos Utiles/Sanchez.Lucas.Gabriel.2C.TP3/Clases Abstractas/Persona.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Abstractas
+{
+    public abstract class Persona
+    {
+        private string _apellido;
+        private int _dni;
+        private ENacionalidad _nacionalidad;
+        private string _nombre;
+
+        public enum ENacionalidad {Argentino, Extranjero};
+
+        #region Propiedades
+        public string Apellido
+        {
+            get
+            { 
+                return this._apellido; 
+            }
+
+            set 
+            { 
+                this._apellido = value; 
+            }
+        }
+
+        public int DNI
+        {
+            get 
+            { 
+                return this._dni; 
+            }
+
+            set 
+            { 
+                this._dni = value; 
+            }
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get 
+            { 
+                return this._nacionalidad; 
+            }
+
+            set 
+            { 
+                this._nacionalidad = value; 
+            }
+        }
+
+        public string Nombre
+        {
+            get 
+            { 
+                return this._nombre; 
+            }
+
+            set 
+            { 
+                this._nombre = value; 
+            }
+        }
+
+        public string StringToDNI 
+        {
+            set
+            {
+                ;
+            }
+        }
+        #endregion
+
+        public Persona()
+        {
+        }
+
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+        : this(nombre,apellido,nacionalidad)
+        {
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+        {
+        }
+
+
+
+    }
+}
---
+++ Archivos Utiles/Sanchez.Lucas.Gabriel.2C.TP3/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Validación solo letras.
+            //string xD = "hola5";
+
+            //bool aux = false;
+
+            //foreach (char item in xD)
+            //{
+            //    if (!(char.IsLetter(item)))
+            //        aux = true;
+            //}
+
+            //if (aux)
+            //    Console.WriteLine("No Son Solo Letras");
+            //else
+            //    Console.WriteLine("Son Solo Letras");
+                
+
+
+            Console.ReadLine();
+        }
+    }
+}
