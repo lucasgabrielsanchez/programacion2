d7e1395a86eb049c04f75befcf0bce7c7b824c16
alumno <alumno>
2017-06-08 22:15:50 -0300

    Clase 18 Generics y Test Unitario

--- miSolución/Progamacion 2/Sanchez.Lucas/Clase18Consola/Program.cs
+++ miSolución/Progamacion 2/Sanchez.Lucas/Clase18Consola/Program.cs
@@ -50,6 +50,9 @@ namespace Clase18Consola
             }
             Console.WriteLine(da);
 
+            Console.ReadLine();
+            Console.Clear();
+
 
             Bicicleta b1 = new Bicicleta("Shimano", 26f, 4500f);
             Bicicleta b2 = new Bicicleta("Mountain", 25f, 4000f);
@@ -64,6 +67,9 @@ namespace Clase18Consola
             ddb.Agregar(b4);
 
             Console.WriteLine(ddb);
+
+            Console.ReadLine();
+            Console.Clear();
 
             Cocina c1 = new Cocina(111, 12300, false);
             Cocina c2 = new Cocina(112, 15000, true);
@@ -82,9 +88,54 @@ namespace Clase18Consola
                 Console.WriteLine("No se pudo remover el item!!!");
             }
             Console.WriteLine(dc);
+           
             Console.ReadLine();
+            Console.Clear();
 
+            //terminar esta parte
+            Cocina cc1 = new Cocina(111, 12300, false);
+            Cocina cc2 = new Cocina(112, 15000, true);
+            Cocina cc3 = new Cocina(113, 5600, false);
+            Auto aa1 = new Auto("Rojo", "Ferrari");
+            Auto aa2 = new Auto("Amarillo", "Porche");
+            Auto aa3 = new Auto("Negro", "BMW");
+            Auto aa4 = new Auto("Verde", "Ford");
+            Deposito_Genérico<Cocina> ddc = new Deposito_Genérico<Cocina>(5);
+            Deposito_Genérico<Auto> dda = new Deposito_Genérico<Auto>(3);
+            dc.Agregar(c1);
+            dc.Agregar(c2);
+            if (!(dc + c3))
+            {
+            Console.WriteLine("No se pudo agregar el item!!!");
+            }
+            if ((da + a1))
+            {
+            Console.WriteLine("Se ha agregado el item!!!");
+            }
+            da.Agregar(a2);
+            da.Agregar(a3);
+            if (!da.Agregar(a4))
+            {
+            Console.WriteLine("No se pudo agregar el item!!!");
+            }
+            Console.WriteLine(dc);
+            Console.WriteLine(da);
+            dc.Remover(c2);
+            if (!(dc - c2))
+            {
+            Console.WriteLine("No se pudo remover el item!!!");
+            }
+            da.Remover(a2);
+            if (!(da - a4))
+            {
+            Console.WriteLine("No se pudo remover el item!!!");
+            }
+            Console.WriteLine(dc);
+            Console.WriteLine(da);
+            
             Console.ReadLine();
+           
+ 
         }
     }
 }
---
+++ miSolución/Progamacion 2/Sanchez.Lucas/Clase18Librería/Deposito(Genérico).cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase18Librería
+{
+    public class Deposito_Genérico <T> //Una clase con esta firma se transforma en genérica, luego se le podrá pasar cualquier tipo;
+    {
+        private int _capacidadMaxima;
+        private List<T> _lista;
+
+        public Deposito_Genérico(int capacidad)
+        {
+            this._lista = new List<T>(); 
+            this._capacidadMaxima = capacidad;
+        }
+
+        public static bool operator +(Deposito_Genérico<T> d, T a)
+        {
+            bool aux = false;
+
+            if (d._lista.Count < d._capacidadMaxima)
+            {
+                d._lista.Add(a);
+                aux = true;
+            }
+
+            return aux;
+        }
+
+        private int GetIndice(T a)
+        {
+            int aux = -1;
+
+            for (int i = 0; i < this._lista.Count(); i++)
+            {
+                //if (this._lista[i] == a)
+                if (this._lista.Equals(a))//en una clase genérica no se puede usar los operadores sobrecargados
+                                          //por eso utilizamos el Equals
+                {
+                    aux = i;
+                    break;
+                }
+            }
+
+            return aux;
+        }
+
+
+        public static bool operator -(Deposito_Genérico<T> d, T a)
+        {
+            bool aux = false;
+            int indice = d.GetIndice(a);
+
+            if(indice >-1)
+            {
+                d._lista.RemoveAt(indice);
+                aux = true;
+            }
+
+            return aux;
+        }
+
+        public bool Agregar(T a)
+        {
+            return (this + a);
+        }
+
+        public bool Remover(T a)
+        {
+            return (this - a);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendLine("Capacidad máxima: " + this._capacidadMaxima);
+            sb.AppendLine("Listado de cocinas:");
+
+            foreach (T item in this._lista)
+            {
+                sb.Append(item);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
